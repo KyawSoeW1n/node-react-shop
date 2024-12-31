73f367c1a4e55d9f7af17ff18cd04acb26f22dda
KyawSoeW1n <kyawsoewin.august@gmail.com>
2024-12-31 18:17:49 +0700

    Add Pagination

--- api/routes/Product.js
+++ api/routes/Product.js
@@ -4,10 +4,11 @@ const asyncHandler = require("express-async-handler");
 const Product = require("../models/Product");
 
 const successHandler = require('../middleware/SuccessHandler');
+
 productRoute.get(
   "/",
   asyncHandler(async (req, res, next) => {
-    const { search, sort, sortBy } = req.query;
+    const { search, sort, sortBy, offset = 0, limit = 10 } = req.query;
 
     // Create a filter object for search
     const filter = search ? { name: { $regex: search, $options: 'i' } } : {};
@@ -18,7 +19,14 @@ productRoute.get(
       sortOption[sortBy] = sort === 'asc' ? 1 : -1;
     }
 
-    const products = await Product.find(filter).sort(sortOption);
+    // Calculate pagination values
+    const offsetValue = parseInt(offset, 10);
+    const limitValue = parseInt(limit, 10);
+
+    const products = await Product.find(filter)
+      .sort(sortOption)
+      .skip(offsetValue)
+      .limit(limitValue);
 
     res.status(200).json(successHandler(products));
   })
